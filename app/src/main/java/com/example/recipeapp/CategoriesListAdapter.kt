57d8672243efc76f0871fcb1bebb34d8a0290313
zhenya <zhenya1032@gmail.com>
2024-11-26 21:19:41 +0300

    RA-08 правки после ревью

--- app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
@@ -3,11 +3,11 @@ package com.example.recipeapp
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recipeapp.databinding.ItemCategoryBinding
 import com.example.recipeapp.databinding.ItemCategoryBinding.bind
 import java.io.IOException
 
@@ -15,8 +15,7 @@ import java.io.IOException
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val binding = bind(itemView)
+    class ViewHolder(binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
         val itemTitle: TextView = binding.tvTitle
         val itemDescription: TextView = binding.tvDescription
         val itemImage: ImageView = binding.imageCategory
@@ -25,8 +24,9 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
         val view = inflater.inflate(R.layout.item_category, viewGroup, false)
+        val binding = bind(view)
 
-        return ViewHolder(view)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -43,6 +43,8 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
         viewHolder.itemTitle.text = positionData.title
         viewHolder.itemDescription.text = positionData.description
         viewHolder.itemImage.setImageDrawable(drawable)
+        viewHolder.itemImage.contentDescription =
+            "Изображение категории ${positionData.title.lowercase()}"
     }
 
     override fun getItemCount() = dataSet.size
