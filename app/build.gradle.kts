96b3764243a6ed98cdad8fbd86d4b1652a52a7e2
zhenya <zhenya1032@gmail.com>
2025-02-22 19:55:27 +0300

    RA-28 реализация jetpack navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -40,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -50,4 +50,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/example/recipeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/ui/MainActivity.kt
@@ -2,13 +2,9 @@ package com.example.recipeapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
+import androidx.navigation.findNavController
 import com.example.recipeapp.R
 import com.example.recipeapp.databinding.ActivityMainBinding
-import com.example.recipeapp.ui.categories.CategoriesListFragment
-import com.example.recipeapp.ui.recipes.favorites.FavoritesFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,39 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<CategoriesListFragment>(R.id.mainContainer, CATEGORIES_TAG)
-            }
-        }
         setContentView(binding.root)
 
         binding.btnFavorites.setOnClickListener {
-            val favoritesFragment = supportFragmentManager.findFragmentByTag(FAVORITES_TAG)
-            if (favoritesFragment == null || !favoritesFragment.isVisible) {
-                supportFragmentManager.commit {
-                    replace<FavoritesFragment>(R.id.mainContainer, FAVORITES_TAG)
-                    setReorderingAllowed(true)
-                    addToBackStack(null)
-                }
-            }
+            findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
         }
 
         binding.btnCategories.setOnClickListener {
-            val categoriesFragment = supportFragmentManager.findFragmentByTag(CATEGORIES_TAG)
-            if (categoriesFragment == null || !categoriesFragment.isVisible) {
-                supportFragmentManager.commit {
-                    replace<CategoriesListFragment>(R.id.mainContainer, CATEGORIES_TAG)
-                    setReorderingAllowed(true)
-                    addToBackStack(null)
-                }
-            }
+            findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment)
         }
-    }
-
-    companion object {
-        private const val FAVORITES_TAG = "Favorites"
-        private const val CATEGORIES_TAG = "Categories"
     }
 }
