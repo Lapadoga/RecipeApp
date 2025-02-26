3f4fb92dbe7fce5301269a3a5ec37372de8b2596
zhenya <zhenya1032@gmail.com>
2025-02-26 21:00:59 +0300

    RA-29 правки после ревью

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
     kotlin("plugin.serialization") version "2.0.21"
-    id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.kotlin.safeargs)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-}
-buildscript {
-    repositories {
-        google()
-    }
-    dependencies {
-        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
-    }
+    alias(libs.plugins.kotlin.safeargs) apply false
 }
